e309b90566a6e7e76e1952af1440dbece3eb3eee
Elisa <94677942+ElisaUnivr@users.noreply.github.com>
2023-04-27 17:29:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from text_extractor import TextExtractor
 from context_retriever import ContextRetriever
 from answer_retriever import AnswerRetriever
 import wikipedia
+import os
 
 lingua = input('Insert the language you want [en , it]:')
 
@@ -18,24 +19,38 @@ print(search[id])
 namePage = search[id]
 contesto = input('Do you want research in the table or in the text? [table , text]: ')
 
+os.chdir('./Desktop/DistilBert-main')
+
 while True:
     originalQuestion = input('Insert the question:').lower()
     if lingua == 'it':
-        textExtractor1 = TextExtractor(namePage, "it", contesto)
+        textExtractor = TextExtractor(namePage, "it", contesto)
         # inizializzo la funzione nlp della lingua italiana
+        
+        try:
+            spacy.load("it_core_news_sm")  # md
+        except:
+            print("Installazione lingua italiana ")
+            os.system("python -m spacy download it_core_news_sm")
         nlp = spacy.load("it_core_news_sm")  # md
+    '''
     else:
-        textExtractor1 = TextExtractor(namePage, "en", contesto)
+        textExtractor = TextExtractor(namePage, "en", contesto)
         # inizializzo la funzione nlp della lingua inglese.
+        try:
+            spacy.load("en_core_web_sm")  # md
+        except:
+            print("English leanguage download")
+            os.system("python -m spacy download en_core_web_sm")
         nlp = spacy.load("en_core_web_sm")  # md
+    '''
 
-    textExtractor1.extract()
-    # textExtractorPipe = TextExtractorPipe()
-    
+
+    textExtractor.extract()
     #Aggiunge un componente alla pipeline di elaborazione.
     nlp.add_pipe('sentencizer')
-    # doc = nlp(textExtractorPipe.extract())
-    doc = nlp(textExtractor1.search())
+    doc = nlp(textExtractor.search())
+
     #itera le frasi in un documenti eliminando il primo e l'ultim carettere
     sentences = [sent.text.strip() for sent in doc.sents]
     sentences = [string for string in sentences if string != ""]
